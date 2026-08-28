8bada796b3eb7814d2a3965ea0ae7894d049f944
Boris Popic <borispopic.business@gmail.com>
2026-08-28 10:50:18 +0200

    Initialize repo

---
+++ src/main/java/org/orvix/auth/OrvixAuthApplication.java
@@ -0,0 +1,13 @@
+package org.orvix.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrvixAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrvixAuthApplication.class, args);
+    }
+
+}
